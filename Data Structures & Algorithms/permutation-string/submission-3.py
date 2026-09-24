a544c3b1ee54ec85ef70032c3c85660a5fcf3fd5
Mia Pham <pham222h@mtholyoke.edu>
2026-09-23 22:15:47 -0400

    Add: permutation-string - submission-3

---
+++ Data Structures & Algorithms/permutation-string/submission-3.py
@@ -0,0 +1,32 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        chars = {}
+        for i in s1:
+            chars[i] = 1 + chars.get(i, 0)
+        l = 0
+        r = 0
+        while r < len(s2):
+            while s2[r] not in chars.keys():
+                # Move l until it is in keys
+                l = r
+                if all(x == 0 for x in chars.values()):
+                    return True
+                # Reset char
+                chars = {}
+                for i in s1:
+                    chars[i] = 1 + chars.get(i, 0)
+                l += 1
+                r += 1
+                if r >= len(s2):
+                    return False
+            chars[s2[r]] -= 1
+            while chars[s2[r]] < 0:
+                chars[s2[l]] += 1
+                l += 1
+            if all(x == 0 for x in chars.values()):
+                return True
+            r += 1
+        return False
+
+
+            
